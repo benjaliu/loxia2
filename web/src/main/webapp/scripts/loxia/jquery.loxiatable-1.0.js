56fa3e06974a6ecd3cc37d25e71ab6959e08a31d
benjaliu <benjaliu@ca9f5868-c45d-11de-9233-4169c94807ec>
2009-11-24 07:12:22 +0000

    table with select almost finished
    git-svn-id: https://loxia2.googlecode.com/svn/trunk@34 ca9f5868-c45d-11de-9233-4169c94807ec

--- web/src/main/webapp/scripts/loxia/jquery.loxiatable-1.0.js
+++ web/src/main/webapp/scripts/loxia/jquery.loxiatable-1.0.js
@@ -1,5 +1,5 @@
 (function($){
-	$.fn.loxiatable = function(settings){
+	$.fn.loxiatable = function(settings){		
 		settings = $.extend({},{
 			sort: "",			
 			page: false,
@@ -12,7 +12,8 @@
 			data: [],
 			form: "",
 			url: "",
-			select: "", // can be blank, single or multi
+			cacheSelect: false,
+			selected: {},
 			images: "images/loxia"
 		},settings);
 		this.each(function(){
@@ -21,22 +22,72 @@
 		});
 		return this;
 	};
+	
+	$.fn.ltInitSelector = function(settings){
+		var $t = $(this);
+		var selectors = [];
+		var selected = settings.selected;
+		var sels = 0;
+		$t.find("thead tr:last th").each(function(){
+			var s = "";
+			var chkbox = $(this).find("input[type='checkbox']");
+			var radbox = $(this).find("input[type='radio']");
+			var name = $(this).attr("name") || $(this).attr("property");
+			if(chkbox.get(0)) s = "+" + name;
+			else if(radbox.get(0)) {
+				radbox.replaceWith("&nbsp;");
+				s = "-" + name;
+			}
+			
+			selectors.push(s);
+			if(s){
+				sels ++;
+				selected["col_"+ (selectors.length-1)] = $.extend({},selected["col_"+ (selectors.length-1)]);
+				$(this).addClass("selector");
+			}
+		});
+		
+		$t.data("cacheSelect", settings.cacheSelect);
+		$t.data("selectCols", sels);
+		$t.data("selectors", selectors);
+		$t.data("selected", selected);
+		return this;
+	};
+	
 	$.fn.ltLoadData = function(data, reloadAll){
 		var $t = $(this);
 		var $tbody = $t.find("tbody:first");
 		var propList = [];
+		
 		$t.find("thead tr:last th").each(function(){
 			propList.push($(this).attr("property"));
 		});
+		
+		var selectors = $t.data("selectors");
 		if(data && data.length && data.length >0){
 			var rowlist = "";
 			for(var dataItem,i=0; (dataItem = data[i]); i++){
 				var row = "<tr>";
 				for(var p,j=0;p=propList[j];j++){
-					var value = $.loxia.getObject(p,dataItem);
-					value = (value == undefined || value == null ||
-							($.loxia.isString(value) && !value)) ? "&nbsp;" : value;
-					row += "<td>" + value + "</td>";					
+					var value = $.loxia.getObject(p,dataItem);					
+					if(selectors[j]){
+						var selected = $t.data("selected")["col_"+j];						
+						value = (value == undefined || value == null ||
+								($.loxia.isString(value) && !value)) ? "" : "" + value;
+						value = value.replace(/\./ig,"_");
+						var strSelected = (value in selected)? " checked" : "";
+						var name = selectors[j].substring(1);
+						if(selectors[j].charAt(0) == '+'){
+							row += "<td><input type='checkbox'" + strSelected + " name='" + name + "' value='" + value + "'/></td>";
+						}else if(selectors[j].charAt(0) == '-'){
+							row += "<td><input type='radio'" + strSelected + " name='" + name + "' value='" + value + "'/></td>";
+						}else
+							throw new Exception("Load Data for Selector Error.");
+					}else{
+						value = (value == undefined || value == null ||
+								($.loxia.isString(value) && !value)) ? "&nbsp;" : value;
+						row += "<td>" + value + "</td>";
+					}
 				}
 				row += "</tr>";
 				rowlist += row;
@@ -46,6 +97,7 @@
 			}else
 				$tbody.append(rowlist);
 		}
+		$t.trigger("dataReloadedEvent",[data]);
 	};
 	$.fn.ltInit = function(settings){
 		var $t = $(this);	
@@ -54,15 +106,69 @@
 			$t.data("url", settings.url);
 		if(settings.form)
 			$t.data("form", settings.form);
-		if(settings.select)
-			$t.data("select", settings.select);
 		if(settings.images)
 			$t.data("images", settings.images);
 		
+		$t.ltInitSelector(settings);
 		$t.ltLoadData(settings.data, false);
 		$t.ltInitStyle(settings);
 		$t.ltInitHeadAction(settings);
 		$t.ltInitPager(settings);
+		
+		$("tbody:first .selector input", $t).livequery(function(){
+			$(this).click(function(){
+				if($t.data("selectCols") == 1)
+					$(this).parents("tr").toggleClass("selected");
+				var selected = $t.data("selected")["col_" + $(this).parents("td").get(0).cellIndex];
+				var value = $(this).val().replace(/\./ig,"_");
+				if($(this).is(":checked")){
+					if($(this).is("input[type='radio']")){						
+						if(value in selected){
+							delete selected[value];
+							$(this).attr("checked",false);
+						}else{
+							for(key in selected)
+								delete selected[key];
+							selected[value] = value;
+						}
+					}else{
+						selected[value] = value;
+					}
+				}else{
+					delete selected[value];
+				}
+				//console.dir($t.data("selected"));
+				$t.trigger("selectChangedEvent",[[$(this),selected]]);
+			});
+		});
+		
+		$("thead .selector input",$t).click(function(){
+			var i = $(this).parents("th").get(0).cellIndex;
+			var needStyle = ($t.data("selectCols") == 1);
+			var selected = $t.data("selected")["col_" + i];
+			var checked = $(this).is(":checked");
+			
+			var changed = false;
+			$t.find("tbody:first tr").find("td:eq(" + i + ") input").each(function(){
+				var value = $(this).val().replace(/\./ig,"_");
+				console.log($(this).is(":checked"));
+				if($(this).is(":checked") != checked){
+					changed = true;
+					$(this).attr("checked", checked);
+					
+					if(checked)
+						selected[value] = value;
+					else
+						delete selected[value];
+					
+					if(needStyle)
+						$(this).parents("tr").toggleClass("selected");
+				}
+			});
+			
+			if(changed)
+				$t.trigger("selectChangedEvent",[[$(this),selected]]);
+		});
 	};
 	
 	$.fn.ltInitStyle = function(settings){
@@ -84,14 +190,17 @@
 					else if(sortStatus[1].toLowerCase() == "desc") sortClass = "sort-desc";
 				}		
 			}
+			var className = $(this).hasClass("selector") ? "selector" : "";
 			$(this).addClass(sortClass).addClass("col-" + i);
 			$(this).html("<div class='th-col-" + i + "'>" + $(this).html() + "</div>");
-			$t.find("tbody:first tr").find("td:eq(" + i + ")").addClass("col-" + i);
+			$t.find("tbody:first tr").find("td:eq(" + i + ")").addClass(className + " col-" + i);
 			$t.data("cols", ++i);
 		});
 		$t.find('tbody:first tr:odd').addClass("odd");
 		$t.find('tbody:first tr:even').addClass("even");
 			
+		if($t.data("selectCols") == 1)
+			$t.find("tbody:first input:checked").parents("tr").addClass("selected");
 		return this;
 	}
 	
@@ -99,6 +208,12 @@
 		var $t = $(this);
 		var imagePath = settings.images || $t.data("images");
 		$t.find('.ltPager .load img').attr('src', imagePath+'/load.gif');
+		if(!$t.data("cacheSelect")){
+			var selected = $t.data("selected");
+			for(key in selected){
+				selected[key] = {};
+			}
+		}
 		settings = $.extend({
 			error : function(XMLHttpRequest, textStatus, errorThrown){
 			//TODO exception handling here
@@ -108,7 +223,7 @@
 			//console.dir(exception);
 			}
 		},settings);
-		console.dir(settings);
+		//console.dir(settings);
 		var url = settings.url || $t.data("url");
 		$.loxia.asyncXhr(url, settings, function(data, textStatus){
 			//console.dir(data);
@@ -132,12 +247,19 @@
 							if(sort == sortStatus[0]){
 								$(this).addClass("sort-" + sortStatus[1]);
 							}		
-						}						
+						}		
+						
+						var className = $(this).hasClass("selector") ? "selector" : "";
+						$t.find("tbody:first tr").find("td:eq(" + i + ")").addClass(className + " col-" + i);
+						$t.data("cols", ++i);
 					});
 				}
 
 				$t.find('tbody:first tr:odd').addClass("odd");
 				$t.find('tbody:first tr:even').addClass("even");
+				
+				if($t.data("selectCols") == 1)
+					$t.find("tbody:first input:checked").parents("tr").addClass("selected");
 				
 				if(data.page)
 					$t.ltSetPager(data);
@@ -150,9 +272,9 @@
 		var $t = $(this);
 		$("thead tr:last th", $t).livequery(function(){
 			$(this).hover(function(){
-				$(this).addClass("hover");
+				$(this).toggleClass("hover");
 			},function(){
-				$(this).removeClass("hover");
+				$(this).toggleClass("hover");
 			});
 			
 			$(this).click(function(){
