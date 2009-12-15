b9ef6302abcd3145a9902f0bd51716c2826ffc27
benjaliu <benjaliu@ca9f5868-c45d-11de-9233-4169c94807ec>
2009-12-15 05:45:39 +0000

    add script minifier
    git-svn-id: https://loxia2.googlecode.com/svn/trunk@90 ca9f5868-c45d-11de-9233-4169c94807ec

---
+++ web/merge.bat
@@ -0,0 +1,9 @@
+cd target\scripts
+type jquery.loxiacore-1.1.min.js >> jquery.loxia-1.1.min.js
+type jquery.loxiatooltip-1.1.min.js >> jquery.loxia-1.1.min.js
+type jquery.loxiabutton-1.1.min.js >> jquery.loxia-1.1.min.js
+type jquery.loxiainput-1.1.min.js >> jquery.loxia-1.1.min.js
+type jquery.loxiaselect-1.1.min.js >> jquery.loxia-1.1.min.js
+type jquery.loxiadropdown-1.1.min.js >> jquery.loxia-1.1.min.js
+type jquery.loxiadialog-1.1.min.js >> jquery.loxia-1.1.min.js
+type jquery.loxiatable-1.1.min.js >> jquery.loxia-1.1.min.js
