53e5b79135e541070d41e4681f86c45363761fcf
benjaliu <benjaliu@ca9f5868-c45d-11de-9233-4169c94807ec>
2009-12-23 16:25:51 +0000

    add date type convertor
    git-svn-id: https://loxia2.googlecode.com/svn/trunk@125 ca9f5868-c45d-11de-9233-4169c94807ec

---
+++ web/src/main/java/cn/benjamin/loxia/web/DateTypeConvertor.java
@@ -0,0 +1,38 @@
+package cn.benjamin.loxia.web;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+
+import org.apache.struts2.util.StrutsTypeConverter;
+
+import com.opensymphony.xwork2.conversion.TypeConversionException;
+
+public class DateTypeConvertor extends StrutsTypeConverter {
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Object convertFromString(Map context, String[] values, Class toClass) {
+		if(values == null || values.length == 0) return null;
+		String dateFormat = LoxiaWebSettings.getInstance().get(LoxiaWebConstants.DATE_PATTERN);
+		DateFormat df = new SimpleDateFormat(dateFormat);
+		try {
+			return df.parse(values[0]);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			throw new TypeConversionException(values[0] + " is not one valid date.");
+		}		
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public String convertToString(Map context, Object o) {
+		if(!(o instanceof Date)) throw new TypeConversionException();
+		String dateFormat = LoxiaWebSettings.getInstance().get(LoxiaWebConstants.DATE_PATTERN);
+		DateFormat df = new SimpleDateFormat(dateFormat);
+		return df.format((Date)o);
+	}
+
+}
---
+++ web/src/main/java/cn/benjamin/loxia/web/LoxiaWebConstants.java
@@ -0,0 +1,5 @@
+package cn.benjamin.loxia.web;
+
+public interface LoxiaWebConstants {
+	public static final String DATE_PATTERN = "date.pattern";
+}
