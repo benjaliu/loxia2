8522bb59ace92f31212aa300a880d0b3042b946c
benjaliu <benjaliu@ca9f5868-c45d-11de-9233-4169c94807ec>
2009-12-29 01:10:08 +0000

    fix select readonly
    git-svn-id: https://loxia2.googlecode.com/svn/trunk@133 ca9f5868-c45d-11de-9233-4169c94807ec

--- web/src/main/webapp/scripts/loxia/jquery.loxiaselect-1.1.js
+++ web/src/main/webapp/scripts/loxia/jquery.loxiaselect-1.1.js
@@ -1,6 +1,7 @@
 (function($) {
 	
 	var loxiaSelect = $.extend({}, loxia.loxiaWidget, {
+		readOnly: false,
 		getBaseClass : function(){
 			return "loxiaselect";
 		},
@@ -63,7 +64,45 @@
 				this._initSelect();
 			}else
 				throw new Error("Wrong Dom Type for Input");
-		}
+		},		
+		_getDimensions: function(el){
+		    var ret = {};
+
+		    // The multiple acquisitions of the CSS styles are required to cover any border and padding the elements may have.
+		    // The Ternary (parseInt(...) || 0) statements fix a bug in IE6 where it returns NaN,
+		    //  which doesn't play nicely when adding to numbers...
+		    ret.width = el.width()
+		      + (parseInt(el.css('borderLeftWidth')) || 0)
+		      + (parseInt(el.css('borderRightWidth')) || 0)
+		      + (parseInt(el.css('padding-left')) || 0)
+		      + (parseInt(el.css('padding-right')) || 0);
+		    ret.height = el.height()
+		      + (parseInt(el.css('borderTopWidth')) || 0)
+		      + (parseInt(el.css('borderBottomWidth')) || 0)
+		      + (parseInt(el.css('padding-bottom')) || 0)
+		      + (parseInt(el.css('padding-bottom')) || 0);
+		    var offsets = el.offset();
+		    ret.left = offsets.left;
+		    ret.top = offsets.top;
+
+		    return ret;
+		},
+		setReadonly : function(state){
+			var overlay = $('<div class="readonly-overlay"></div>');
+			if(this.readOnly == state) return;
+			this.readOnly = state;
+			if(state){				
+				this.element.bind('focus', this.unfocus).after(overlay);
+				overlay.css($.extend({'position':'absolute',
+					'z-index':'999'}, this._getDimensions(this.element)));				    
+				if($.browser.version == '6.0' && $.browser.msie) this.element.css('visibility', 'hidden');
+			}else{
+				overlay = this.element.unbind('focus', this.unfocus).next();
+				overlay.remove();
+				if($.browser.version == '6.0' && $.browser.msie) this.element.css('visibility', 'visible');
+			}
+		},
+		unfocus : function(e){$(e.currentTarget).blur();}
 	});
 	
 	$.widget("ui.loxiaselect", loxiaSelect); 
