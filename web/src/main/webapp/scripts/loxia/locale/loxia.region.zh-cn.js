066be295f599c4e8902251dbc35736e3ebcd7703
benjaliu <benjaliu@ca9f5868-c45d-11de-9233-4169c94807ec>
2010-03-15 08:49:10 +0000

    append something
    git-svn-id: https://loxia2.googlecode.com/svn/trunk@224 ca9f5868-c45d-11de-9233-4169c94807ec

---
+++ web/src/main/webapp/scripts/loxia/locale/loxia.region.zh-cn.js
@@ -0,0 +1,24 @@
+loxia.regional['zh-cn'] = {
+			INVALID_EMPTY_DATA : "当前输入内容不能为空",
+			INVALID_DATA : "当前输入非法",
+			INVALID_NUMBER : "当前输入不是一个合法的数字或精度超出",
+			INVALID_DATE : "当前输入不是一个合法的日期",
+			DATA_EXCEED_RANGE : "当前输入不在允许的输入范围内",
+			
+			VALIDATE_FIELD_ERROR : "共有 {0}处错误，请检查",
+			VALIDATE_FORM_ERROR : "表单提交失败：",
+			
+			TABLE_PER_PAGE : "每页",
+			TABLE_PAGER_FIRST : "首页",
+			TABLE_PAGER_PREVIOUS : "上一页",
+			TABLE_PAGER_NEXT : "下一页",
+			TABLE_PAGER_LAST : "末页",
+			TABLE_PAGER_GOTO : "转到",
+			TABLE_PAGER_RELOAD : "刷新",			
+			TABLE_PAGE : "页",
+			TABLE_PAGE_INFO : "共 {0}条记录",
+			TABLE_PAGE_ERROR_OCCURS : "表格读取时发生错误",
+			TABLE_PAGE_RELOAD : "表格记录已成功刷新",
+			TABLE_BARBTN_ADD : "新增记录",
+			TABLE_BARBTN_DELETE : "删除选中记录"
+		};		
