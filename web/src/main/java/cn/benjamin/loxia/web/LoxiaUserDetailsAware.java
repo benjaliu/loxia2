d21fc5f52fa0c37a851bd725a37b2b247e099649
benjaliu <benjaliu@ca9f5868-c45d-11de-9233-4169c94807ec>
2010-03-15 08:47:07 +0000

    refactor structure
    git-svn-id: https://loxia2.googlecode.com/svn/trunk@220 ca9f5868-c45d-11de-9233-4169c94807ec

--- web/src/main/java/cn/benjamin/loxia/web/LoxiaUserDetailsAware.java
+++
@@ -1,7 +0,0 @@
-package cn.benjamin.loxia.web;
-
-import cn.benjamin.loxia.security.LoxiaUserDetails;
-
-public interface LoxiaUserDetailsAware {
-	void setLoxiaUserDetails(LoxiaUserDetails userDetails);
-}
--- web/src/main/java/cn/benjamin/loxia/web/LoxiaWebConstants.java
+++
@@ -1,5 +0,0 @@
-package cn.benjamin.loxia.web;
-
-public interface LoxiaWebConstants {
-	public static final String DATE_PATTERN = "date.pattern";
-}
