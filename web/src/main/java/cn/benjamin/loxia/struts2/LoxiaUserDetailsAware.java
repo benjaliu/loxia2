cd8d8a9e88c1fbad2defd19a84ef8d2e14682354
benjaliu <benjaliu@ca9f5868-c45d-11de-9233-4169c94807ec>
2010-06-21 05:56:33 +0000

    refactor loxia2
    git-svn-id: https://loxia2.googlecode.com/svn/trunk@369 ca9f5868-c45d-11de-9233-4169c94807ec

--- web/src/main/java/cn/benjamin/loxia/struts2/LoxiaUserDetailsAware.java
+++
@@ -1,7 +0,0 @@
-package cn.benjamin.loxia.struts2;
-
-import cn.benjamin.loxia.security.LoxiaUserDetails;
-
-public interface LoxiaUserDetailsAware {
-	void setLoxiaUserDetails(LoxiaUserDetails userDetails);
-}
--- web/src/main/java/cn/benjamin/loxia/web/LoxiaWebConstants.java
+++
@@ -1,5 +0,0 @@
-package cn.benjamin.loxia.web;
-
-public interface LoxiaWebConstants {
-	public static final String JSON_FILTER_STR = "jsonFilterString";
-}
