48c07f55f26ec0f5ed21882cdce90e0c2e518a42
benjaliu@gmail.com <benjaliu@gmail.com@ca9f5868-c45d-11de-9233-4169c94807ec>
2012-03-13 03:21:59 +0000

    change readwrite decision method: previously it is decided by transactional annotation, currently is transaction attribute which is parsed by spring
    git-svn-id: https://loxia2.googlecode.com/svn/trunk@1040 ca9f5868-c45d-11de-9233-4169c94807ec

--- core/src/main/java/loxia/aspect/ReadWriteDataSourceAspect.java
+++ core/src/main/java/loxia/aspect/ReadWriteDataSourceAspect.java
@@ -1,7 +1,5 @@
 package loxia.aspect;
 
-import java.lang.reflect.Method;
-
 import loxia.dao.ReadWriteStatusHolder;
 import loxia.dao.ReadWriteSupport;
 
@@ -11,28 +9,31 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Ordered;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAttribute;
+import org.springframework.transaction.interceptor.TransactionAttributeSource;
 
 @Aspect
 public class ReadWriteDataSourceAspect implements Ordered{
 	protected static final Logger logger = LoggerFactory.getLogger(ReadWriteDataSourceAspect.class);
 	
+	@Autowired
+	TransactionAttributeSource transactionAttributeSouce;
+	
 	@Around("this(loxia.dao.ReadWriteSupport)")
-	public Object doQuery(ProceedingJoinPoint pjp) throws Throwable{			
-		MethodSignature ms = (MethodSignature)pjp.getSignature();
-		Method m = pjp.getTarget().getClass().getMethod(ms.getMethod().getName(), ms.getParameterTypes());
-		logger.debug("determine datasource for query:{}.{}",ms.getDeclaringType().getName(),ms.getMethod().getName());
-		Transactional tx = m.getAnnotation(Transactional.class);
-		
-		if(tx == null){
-			logger.debug("Transaction annotation is not found at method.");
-			tx = pjp.getTarget().getClass().getAnnotation(Transactional.class);			
+	public Object doQuery(ProceedingJoinPoint pjp) throws Throwable{	
+		if(ReadWriteStatusHolder.getReadWriteStatus() != null){
+			return pjp.proceed(pjp.getArgs());
 		}
-		logger.debug("Current operation's transaction status: {}-{}", tx==null?"F":"T", tx!=null&&tx.readOnly()?"R":"W");
-		boolean needSet = (tx != null && ReadWriteStatusHolder.getReadWriteStatus() == null);
-		if(needSet){
-			ReadWriteStatusHolder.setReadWriteStatus(tx.readOnly() ? ReadWriteSupport.READ: ReadWriteSupport.WRITE);
+		MethodSignature ms = (MethodSignature)pjp.getSignature();
+		logger.debug("determine datasource for query:{}.{}",ms.getDeclaringType().getName(),ms.getMethod().getName());
+		TransactionAttribute ta = transactionAttributeSouce.getTransactionAttribute(ms.getMethod(), pjp.getTarget().getClass());		
+		
+		logger.debug("Current operation's transaction status: {}", ta == null ? "null": ta.toString());
+		
+		if(ta != null){
+			ReadWriteStatusHolder.setReadWriteStatus((ta != null && ta.isReadOnly()) ? ReadWriteSupport.READ: ReadWriteSupport.WRITE);
 		}
 		try {
 			Object rtn = pjp.proceed(pjp.getArgs());
@@ -40,7 +41,7 @@ public class ReadWriteDataSourceAspect implements Ordered{
 		} catch (Throwable e) {
 			throw e;
 		} finally {
-			if(needSet){
+			if(ta != null){
 				ReadWriteStatusHolder.clearReadWriteStatus();
 				logger.debug("Clear Read/Write Status");
 			}
